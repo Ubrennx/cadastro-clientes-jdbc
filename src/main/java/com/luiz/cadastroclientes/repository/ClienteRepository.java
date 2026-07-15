a925f15b256398a90a97d7966892bb61a10f470c
Brenno <ubrennz@gmail.comm>
2026-07-15 14:18:49 -0300

    refactor: removido import

--- src/main/java/com/luiz/cadastroclientes/repository/ClienteRepository.java
+++ src/main/java/com/luiz/cadastroclientes/repository/ClienteRepository.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.sql.Types;
 import java.util.List;
 import java.util.Optional;
