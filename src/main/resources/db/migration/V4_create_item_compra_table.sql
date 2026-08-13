b1f96cfddac0d341a5b9dcfeb80a4e5883d93a4c
Luiz Henrique <hluiz1505@gmail.com>
2026-08-13 16:20:54 -0300

    fix: correção dos nomes das migrations Flyway

--- src/main/resources/db/migration/V4_create_item_compra_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS item_compra (
-    compra_id       BIGINT NOT NULL REFERENCES compra (id),
-    produto_id      BIGINT NOT NULL REFERENCES produto (id),
-    quantidade      INTEGER NOT NULL,
-    preco_unitario  DOUBLE PRECISION NOT NULL,
-    PRIMARY KEY (compra_id, produto_id)
-);
